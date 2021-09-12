9a405e401d2fb22187f44aa21d2d26b519e8bc3a
Your Name <you@example.com>
2021-09-12 16:11:32 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var navslide = document.getElementById("nav_links")
+var burger = document.getElementById("slider")
+var close = document.getElementById("times")
+
+function slide() {
+    navslide.style.left = "60%";
+}
+
+function closing() {
+    navslide.style.left = "115%";
+}
