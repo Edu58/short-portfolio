465190466a51d6ff9ecad7d3e29f23f3baca73b3
Your Name <you@example.com>
2021-09-16 11:22:21 +0300

    dark mode

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var navslide = document.getElementById("nav_links")
 var burger = document.getElementById("slider")
 var close = document.getElementById("times")
+var bodies = document.getElementById("everything")
 
 function slide() {
     navslide.style.left = "60%";
@@ -8,4 +9,9 @@ function slide() {
 
 function closing() {
     navslide.style.left = "115%";
+}
+
+function darkmode() {
+    document.body.style.background = "#212020"
+    document.body.style.color = "#fff"
 }
